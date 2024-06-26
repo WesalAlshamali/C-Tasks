9ada3ca43c41f0e88461dcd32bb55b4da67ccb5e
WesalAlshamali <wesalmazen4@gmail.com>
2024-06-26 14:18:29 +0300

    26/6

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-------------------------------Q1---------------------------
+            //Console.WriteLine("Please enter your name:");
+
+            //string userInput = Console.ReadLine();
+
+            //Console.WriteLine("You entered: " + userInput);
+            //Console.ReadKey();
+
+            //-------------------------------Q2---------------------------
+            //double x = 34.5;
+            //Console.WriteLine("Double "+ x);
+            //String y = "wesal";
+            //Console.WriteLine("string "+ y);
+            //char u = 'Q';
+            //Console.WriteLine("char "+ u);
+            //bool z = true;
+            //Console.WriteLine("boolean "+ z);
+            //int p = 12;
+            //Console.WriteLine("int "+ p);
+            //const string w= "hi hi";
+            //Console.WriteLine("const "+ w);
+            //Console.ReadKey();
+
+            //-------------------------------Q3---------------------------
+            //string[] cars = { "Volvo", "BMW", "Ford"};
+            //foreach (string i in cars)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("the array length is "+cars.Length);
+            //Console.ReadKey();
+            //-------------------------------Q4---------------------------
+
+            //Console.WriteLine("Please enter first name:");
+            //string userfname = Console.ReadLine();
+            //Console.WriteLine("Please enter last name:");
+            //string userlname = Console.ReadLine();
+            //Console.WriteLine("Please enter year of birth:");
+            //string age = Console.ReadLine();
+
+            //Console.WriteLine("Your data is: " + userfname+" "+ userlname + " " +age);
+
+            //Console.ReadKey();
+            //-------------------------------Q5---------------------------
+            //int[] arr = new int[10];
+
+            //Console.WriteLine("Input 10 elements in the array:");
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write("element - {0} : ", i);
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.Write("Elements in array are: ");
+            //foreach (int element in arr)
+            //{
+            //    Console.Write(element + " ");
+            //
+            //}
+          //  Console.ReadKey();
+            //-------------------------------Q6-------------------------------
+            //int[] arr = new int[3];
+
+            //Console.WriteLine("Input 3 elements in the array:");
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write("element - {0} : ", i);
+            //    arr[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //Console.Write("the sum is  ");
+          
+            //    Console.Write(arr.Sum());
+            //    Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
